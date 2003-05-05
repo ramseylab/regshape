5746d6d77bd14e2c05693b9b970698fc90fb99ce
Stephen Ramsey <stephen.ramsey@oregonstate.edu>
2003-05-05 22:00:16 +0000

    modified command-line interface for setting default CLASSPATH
    git-svn-id: file:///Users/sramsey/Desktop/DizzyRepo/ISBJava_Dizzy@1828 5021c4a6-a05a-4b22-8634-7b27b77db61f

--- ISBJava_Dizzy/trunk/bolouri/apps/Dizzy/bin/dizzycmd.bat
+++ ISBJava_Dizzy/trunk/bolouri/apps/Dizzy/bin/dizzycmd.bat
@@ -1,2 +1,4 @@
-java -Xmx500mb isb.chem.scripting.MainScriptRunner %1 %2 %3 %4 %5 %6
+set DIZZYDIR=c:\Program Files\Dizzy
+set DIZZYPATH=%DIZZYDIR%\lib\ISBJava.jar;%DIZZYDIR%\lib\SBWCore.jar;%DIZZYDIR%\lib\jfreechart.jar;%DIZZYDIR%\lib\jcommon.jar
+java -Xmx500mb -cp "%CLASSPATH%;%DIZZYPATH%" isb.chem.scripting.MainScriptRunner %1 %2 %3 %4 %5 %6
 
--- ISBJava_Dizzy/trunk/bolouri/apps/Dizzy/bin/dizzycmd.sh
+++ ISBJava_Dizzy/trunk/bolouri/apps/Dizzy/bin/dizzycmd.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-
-java -Xmx500mb 'isb.chem.scripting.MainScriptRunner' $@
+DIZZYDIR=/local/Dizzy
+DIZZYPATH=${DIZZYDIR}/lib/ISBJava.jar:${DIZZYDIR}/lib/SBWCore.jar:${DIZZYDIR}/lib/jfreechart.jar:${DIZZYDIR}/lib/jcommon.jar:${DIZZYDIR}/lib/SBMLValidate.jar
+java -Xmx500mb -cp "${CLASSPATH}:${DIZZYPATH}" 'isb.chem.scripting.MainScriptRunner' $@
 
