2a208d6affe688087c2d5492c04efb2538231a5b
Stephen Ramsey <stephen.ramsey@oregonstate.edu>
2003-08-04 21:27:53 +0000

    added PriorityQueue and IndexedPriorityQueue
    git-svn-id: file:///Users/sramsey/Desktop/DizzyRepo/ISBJava_Dizzy@1945 5021c4a6-a05a-4b22-8634-7b27b77db61f

---
+++ ISBJava_Dizzy/trunk/bolouri/java/org/systemsbiology/util/AbstractComparator.java
@@ -0,0 +1,6 @@
+package org.systemsbiology.util;
+
+public abstract class AbstractComparator
+{
+    public abstract int compare(Object p1, Object p2);
+}
---
+++ ISBJava_Dizzy/trunk/bolouri/java/org/systemsbiology/util/tp/TestPriorityQueue.java
@@ -0,0 +1,42 @@
+package org.systemsbiology.util.tp;
+
+import org.systemsbiology.util.*;
+import java.util.*;
+
+public class TestPriorityQueue
+{
+    public static final void main(String []pArgs)
+    {
+        try
+        {
+            Comparator comp = new Comparator()
+            {
+                public int compare(Object p1, Object p2)
+                {
+                    int retVal = ((Integer) p1).intValue() - ((Integer) p2).intValue();
+                    return(retVal);
+                }
+            };
+        
+            IndexedPriorityQueue pq = new IndexedPriorityQueue(comp);
+            
+            Integer o1 = new Integer(0);
+            Integer o2 = new Integer(1);
+            Integer o5 = new Integer(-13);
+            Integer o3 = new Integer(3);
+            Integer o4 = new Integer(7);
+            pq.offer(o1);
+            pq.offer(o2);
+            pq.offer(o3);
+            pq.offer(o4);
+            pq.offer(o5);
+            pq.update(4, new Integer(10));
+
+            System.out.println(pq);
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace(System.err);
+        }
+    }
+}
