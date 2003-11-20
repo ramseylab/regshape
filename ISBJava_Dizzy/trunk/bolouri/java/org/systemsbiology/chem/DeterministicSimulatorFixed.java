ff1debf3d48724eb3b6a404fa17a751976611450
Stephen Ramsey <stephen.ramsey@oregonstate.edu>
2003-11-20 22:43:59 +0000

    improved so that the simulator can detect the case of insufficient step-size; refactored some code to DeterministicSimulator; added a general framework for passing simulator-specific parameters
    git-svn-id: file:///Users/sramsey/Desktop/DizzyRepo/ISBJava_Dizzy@2088 5021c4a6-a05a-4b22-8634-7b27b77db61f

--- ISBJava_Dizzy/trunk/bolouri/java/org/systemsbiology/chem/DeterministicSimulatorFixed.java
+++ ISBJava_Dizzy/trunk/bolouri/java/org/systemsbiology/chem/DeterministicSimulatorFixed.java
@@ -30,7 +30,7 @@ import java.util.Random;
 public class DeterministicSimulatorFixed extends DeterministicSimulator implements IAliasableClass, ISimulator
 {
     public static final String CLASS_ALIAS = "ODE-RK5-fixed";
-
+    private static final int NUM_ITERATIONS_BEFORE_ERROR_CHECK = 10;
 
     // fixed step-size integrator
     protected final double iterate(SpeciesRateFactorEvaluator pSpeciesRateFactorEvaluator,
@@ -40,23 +40,96 @@ public class DeterministicSimulatorFixed extends DeterministicSimulator implemen
                                    double []pReactionProbabilities,
                                    RKScratchPad pRKScratchPad,
                                    double []pDynamicSymbolValues,
-                                   double []pNewDynamicSymbolValues) throws DataNotFoundException
+                                   double []pNewDynamicSymbolValues) throws DataNotFoundException, SimulationAccuracyException
     {
         double stepSize = pRKScratchPad.stepSize;
 
-        rk4step(pSpeciesRateFactorEvaluator,
-                pSymbolEvaluator,
-                pReactions,
-                pDynamicSymbolAdjustmentVectors,
-                pReactionProbabilities,
-                pRKScratchPad,
-                stepSize,  
-                pDynamicSymbolValues,
-                pNewDynamicSymbolValues);
+
+        int numIterations = pRKScratchPad.numIterations;
+        if(0 != numIterations % NUM_ITERATIONS_BEFORE_ERROR_CHECK)
+        {
+            rk4step(pSpeciesRateFactorEvaluator,
+                    pSymbolEvaluator,
+                    pReactions,
+                    pDynamicSymbolAdjustmentVectors,
+                    pReactionProbabilities,
+                    pRKScratchPad,
+                    stepSize,  
+                    pDynamicSymbolValues,
+                    pNewDynamicSymbolValues);
+        }
+        else
+        {
+            double []yscale = pRKScratchPad.yscale;
+
+            computeScale(pSpeciesRateFactorEvaluator,
+                         pSymbolEvaluator,
+                         pReactions,
+                         pDynamicSymbolAdjustmentVectors,
+                         pReactionProbabilities,
+                         pRKScratchPad,
+                         stepSize,
+                         pDynamicSymbolValues,
+                         yscale);
+
+            double aggregateError = rkqc(pSpeciesRateFactorEvaluator,
+                                         pSymbolEvaluator,
+                                         pReactions,
+                                         pDynamicSymbolAdjustmentVectors,
+                                         pReactionProbabilities,
+                                         pRKScratchPad,
+                                         stepSize,  
+                                         yscale,
+                                         pDynamicSymbolValues,
+                                         pNewDynamicSymbolValues);
+
+            double maxFractionalError = pRKScratchPad.maxFractionalError;
+
+            if(maxFractionalError - aggregateError < 0.0)
+            {
+                throw new SimulationAccuracyException("numeric approximation error exceeded threshold; try a smaller step-size");
+            }
+        }
 
         pSymbolEvaluator.setTime(pSymbolEvaluator.getTime() + stepSize);
 
         return(pSymbolEvaluator.getTime());
+    }
+
+    protected final void setupScratchPad(double pStartTime,
+                                         double pEndTime,
+                                         SimulatorParameters pSimulatorParams, 
+                                         RKScratchPad pRKScratchPad)
+    {
+        Double maxFractionalErrorObj = pSimulatorParams.getMaxAllowedError();
+        if(null != maxFractionalErrorObj)
+        {
+            double maxFractionalError = maxFractionalErrorObj.doubleValue();
+            pRKScratchPad.maxFractionalError = maxFractionalError;
+        }
+        else
+        {
+            throw new IllegalArgumentException("max fractional error must be specified");
+        }
+
+        int minNumSteps;
+        Integer minNumStepsObj = pSimulatorParams.getMinNumSteps();
+        if(null != minNumStepsObj)
+        {
+            minNumSteps = minNumStepsObj.intValue();
+            if(minNumSteps <= 0)
+            {
+                throw new IllegalArgumentException("illegal value for number of steps");
+            }
+        }
+        else
+        {
+            throw new IllegalArgumentException("required minimum number of steps was not provided");
+        }
+
+        double stepSize = (pEndTime - pStartTime) / ((double) minNumSteps);
+
+        pRKScratchPad.stepSize = stepSize;
     }
 
 }
