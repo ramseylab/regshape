3b4622957aafa308fd2a6cc33a5fcca85ca47d40
Stephen Ramsey <stephen.ramsey@oregonstate.edu>
2004-02-06 23:41:50 +0000

    added two libraries
    git-svn-id: file:///Users/sramsey/Desktop/DizzyRepo/ISBJava_Dizzy@2363 5021c4a6-a05a-4b22-8634-7b27b77db61f

--- ISBJava_Dizzy/trunk/bolouri/apps/Dizzy/bin/runmodel.bat
+++ ISBJava_Dizzy/trunk/bolouri/apps/Dizzy/bin/runmodel.bat
@@ -1,4 +1,4 @@
 set ISBCHEMAPPDIR=c:\Program Files\@APPNAME@
-set ISBCHEMAPPCLASSPATH=%ISBCHEMAPPDIR%\lib\ISBJava.jar;%ISBCHEMAPPDIR%\lib\SBWCore.jar;%ISBCHEMAPPDIR%\lib\jfreechart.jar;%ISBCHEMAPPDIR%\lib\jcommon.jar;%ISBCHEMAPPDIR%\lib\SBMLValidate.jar
+set ISBCHEMAPPCLASSPATH=%ISBCHEMAPPDIR%\lib\ISBJava.jar;%ISBCHEMAPPDIR%\lib\SBWCore.jar;%ISBCHEMAPPDIR%\lib\jfreechart.jar;%ISBCHEMAPPDIR%\lib\jcommon.jar;%ISBCHEMAPPDIR%\lib\SBMLValidate.jar;%ISBCHEMAPPDIR%\lib\Jama.jar;%ISBCHEMAPPDIR%\lib\odeToJava.jar
 java @APPJAVARUNTIMEFLAGS@ -cp "%CLASSPATH%;%ISBCHEMAPPCLASSPATH%" org.systemsbiology.chem.app.ModelLoaderApp %1 %2 %3 %4 %5 %6
 
