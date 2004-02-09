f5e475ec91e2dc789463cbd1e3bbe017c6ecac49
Stephen Ramsey <stephen.ramsey@oregonstate.edu>
2004-02-09 16:25:50 +0000

    updated for more command-line arguments
    git-svn-id: file:///Users/sramsey/Desktop/DizzyRepo/ISBJava_Dizzy@2366 5021c4a6-a05a-4b22-8634-7b27b77db61f

--- ISBJava_Dizzy/trunk/bolouri/apps/Dizzy/bin/runmodel.bat
+++ ISBJava_Dizzy/trunk/bolouri/apps/Dizzy/bin/runmodel.bat
@@ -1,4 +1,3 @@
 set ISBCHEMAPPDIR=c:\Program Files\@APPNAME@
 set ISBCHEMAPPCLASSPATH=%ISBCHEMAPPDIR%\lib\ISBJava.jar;%ISBCHEMAPPDIR%\lib\SBWCore.jar;%ISBCHEMAPPDIR%\lib\jfreechart.jar;%ISBCHEMAPPDIR%\lib\jcommon.jar;%ISBCHEMAPPDIR%\lib\SBMLValidate.jar;%ISBCHEMAPPDIR%\lib\Jama.jar;%ISBCHEMAPPDIR%\lib\odeToJava.jar
-java @APPJAVARUNTIMEFLAGS@ -cp "%CLASSPATH%;%ISBCHEMAPPCLASSPATH%" org.systemsbiology.chem.app.SimulationLauncherCommandLine %1 %2 %3 %4 %5 %6
-
+java @APPJAVARUNTIMEFLAGS@ -cp "%CLASSPATH%;%ISBCHEMAPPCLASSPATH%" org.systemsbiology.chem.app.SimulationLauncherCommandLine %1 %2 %3 %4 %5 %6 %7 %8 %9 %10 %11 %12 %13 %14 %15 %16 %17 %18 %19 %20 %21 %22 %23 %24 %25 %26 %27 %28 %29 %30
