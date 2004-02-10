fa9636b9e840c45c470a0a7fc118b90ea15d0b34
No Author <nobody@nowhere.com>
2004-02-10 23:20:29 +0000

    This commit was manufactured by cvs2svn to create tag 'ISBJava-1_4_4'.
    git-svn-id: file:///Users/sramsey/Desktop/DizzyRepo/ISBJava_Dizzy@2374 5021c4a6-a05a-4b22-8634-7b27b77db61f

---
+++ ISBJava_Dizzy/tags/ISBJava-1_4_4/bolouri/java/org/systemsbiology/chem/ISimulator.java
@@ -0,0 +1,46 @@
+package org.systemsbiology.chem;
+/*
+ * Copyright (C) 2003 by Institute for Systems Biology,
+ * Seattle, Washington, USA.  All rights reserved.
+ * 
+ * This source code is distributed under the GNU Lesser 
+ * General Public License, the text of which is available at:
+ *   http://www.gnu.org/copyleft/lesser.html
+ */
+
+import org.systemsbiology.util.DataNotFoundException;
+import org.systemsbiology.util.InvalidInputException;
+
+public interface ISimulator
+{
+    public boolean isInitialized();
+
+    public void initialize(Model pModel, 
+                           SimulationController pSimulationController) throws DataNotFoundException, InvalidInputException;
+
+    public SimulatorParameters getDefaultSimulatorParameters();
+
+    public boolean isStochasticSimulator();
+
+    public boolean allowsInterrupt();
+
+    public long getIterationCounter();
+
+    /**
+     * Conduct a simulation of the dynamics of the {@link Model} passed to the
+     * {@link #initialize(Model,SimulationController)} method.  The end time
+     * must be greater than the start time.  The integer <code>pNumResultsTimePoints</code>
+     * must be greater than zero.  The size of the results arrays must be equal to
+     * one plus <code>pNumResultsTimePoints</code> (the extra element in the arrays is to
+     * hold the initial data).  The parameter <code>pNumResultsTimePoints</code> must
+     * be greater than or equal to 2.
+     *
+     */
+    public void simulate(double pStartTime, 
+                         double pEndTime,
+                         SimulatorParameters pSimulatorParameters,
+                         int pNumResultsTimePoints,
+                         String []pResultsSymbolNames,
+                         double []pRetResultsTimeValues,
+                         Object []pRetResultsSymbolValues) throws DataNotFoundException, IllegalStateException, IllegalArgumentException, SimulationAccuracyException, SimulationFailedException;
+}
