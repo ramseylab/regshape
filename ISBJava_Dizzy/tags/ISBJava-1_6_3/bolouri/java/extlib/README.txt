d35fd904c50d8415305f7d83213c7317847cfe13
No Author <nobody@nowhere.com>
2004-03-26 21:59:52 +0000

    This commit was manufactured by cvs2svn to create tag 'ISBJava-1_6_3'.
    git-svn-id: file:///Users/sramsey/Desktop/DizzyRepo/ISBJava_Dizzy@2546 5021c4a6-a05a-4b22-8634-7b27b77db61f

---
+++ ISBJava_Dizzy/tags/ISBJava-1_6_3/bolouri/java/extlib/README.txt
@@ -0,0 +1,2 @@
+This directory contains JAR files that are needed in order
+to compile and use the ISBJava package.
---
+++ ISBJava_Dizzy/tags/ISBJava-1_6_3/bolouri/java/org/systemsbiology/chem/app/ParserPicker.java
@@ -0,0 +1,114 @@
+package org.systemsbiology.chem.app;
+/*
+ * Copyright (C) 2003 by Institute for Systems Biology,
+ * Seattle, Washington, USA.  All rights reserved.
+ * 
+ * This source code is distributed under the GNU Lesser 
+ * General Public License, the text of which is available at:
+ *   http://www.gnu.org/copyleft/lesser.html
+ */
+
+import java.io.File;
+import java.util.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import org.systemsbiology.util.*;
+import org.systemsbiology.chem.*;
+
+public class ParserPicker
+{
+    private static final String ALIAS_MARKUP_LANGUAGE = "markup-language";
+    private static final String ALIAS_MARKUP_LANGUAGE_MOLECULES = "markup-language-molecules";
+
+    private Component mMainFrame;
+
+    public ParserPicker(Component pMainFrame)
+    {
+        mMainFrame = pMainFrame;
+    }
+
+    private void handleCancel()
+    {
+        SimpleDialog messageDialog = new SimpleDialog(mMainFrame, "Model processing cancelled", 
+                                                      "Your model processing has been cancelled");
+        messageDialog.setMessageType(JOptionPane.INFORMATION_MESSAGE);
+        messageDialog.show();
+    }
+
+    private static java.util.List getModelBuilderAliasesList(ClassRegistry modelBuilderRegistry)
+    {
+        Set modelBuilderAliases = modelBuilderRegistry.getRegistryAliasesCopy();
+        assert (modelBuilderAliases.size() > 0) : "no model builder aliases found";
+        java.util.List modelBuilderAliasesList = new LinkedList(modelBuilderAliases);
+        Collections.sort(modelBuilderAliasesList);
+
+        return(modelBuilderAliasesList);
+    }
+
+    public static String processFileName(String pFileName)
+    {
+        String retModelBuilderAlias = null;
+
+        MainApp app = MainApp.getApp();
+        ClassRegistry modelBuilderRegistry = app.getModelBuilderRegistry();
+
+        java.util.List modelBuilderAliasesList = getModelBuilderAliasesList(modelBuilderRegistry);
+        Iterator modelBuilderAliasesIter = modelBuilderAliasesList.iterator();;
+
+        while(modelBuilderAliasesIter.hasNext())
+        {
+            String modelBuilderAlias = (String) modelBuilderAliasesIter.next();
+            IModelBuilder modelBuilder = null;
+            try
+            {
+                modelBuilder = (IModelBuilder) modelBuilderRegistry.getInstance(modelBuilderAlias);
+                String fileRegex = modelBuilder.getFileRegex();
+                if(pFileName.matches(fileRegex))
+                {
+                    retModelBuilderAlias = modelBuilderAlias;
+                    break;
+                }
+            }
+            catch(DataNotFoundException e)
+            {
+                e.printStackTrace(System.err);
+            }
+        }
+
+        return(retModelBuilderAlias);
+    }
+
+    public String selectParserAliasFromFileName(String pFileName)
+    {
+        String parserAlias = processFileName(pFileName);
+
+        if(null == parserAlias)
+        {
+            MainApp app = MainApp.getApp();
+            ClassRegistry modelBuilderRegistry = app.getModelBuilderRegistry();
+            
+            java.util.List parserAliasesList = getModelBuilderAliasesList(modelBuilderRegistry);
+            Collections.sort(parserAliasesList);
+            Object []parserOptions = parserAliasesList.toArray();
+            File inputFile = new File(pFileName);
+            String shortName = inputFile.getName();
+            SimpleTextArea textArea = new SimpleTextArea("In order to process this model definition, you will need to specify which parser to use.  Which parser plugin do you wish to use?\n[To cancel, just click the close box in the upper-right corner.]");
+            JOptionPane parserOptionPane = new JOptionPane(textArea,
+                                                           JOptionPane.WARNING_MESSAGE,
+                                                           JOptionPane.DEFAULT_OPTION,
+                                                           null,
+                                                           parserOptions);
+            JDialog parserDialog = parserOptionPane.createDialog(mMainFrame, "Please select a parser");
+            parserDialog.show();
+            parserAlias = (String) parserOptionPane.getValue();
+            
+            if(null == parserAlias)
+            {
+                handleCancel();
+            }
+        }
+
+        return(parserAlias);
+    }
+}
