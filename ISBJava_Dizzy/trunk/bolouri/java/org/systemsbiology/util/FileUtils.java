19ce4835353c71c131757e015419940a08d08eb2
Stephen Ramsey <stephen.ramsey@oregonstate.edu>
2004-10-21 20:54:17 +0000

    This class supersedes URLUtils.java; added new getExtension() and removeExtension() methods
    git-svn-id: file:///Users/sramsey/Desktop/DizzyRepo/ISBJava_Dizzy@2706 5021c4a6-a05a-4b22-8634-7b27b77db61f

---
+++ ISBJava_Dizzy/trunk/bolouri/java/org/systemsbiology/util/FileUtils.java
@@ -0,0 +1,72 @@
+package org.systemsbiology.util;
+/*
+ * Copyright (C) 2003 by Institute for Systems Biology,
+ * Seattle, Washington, USA.  All rights reserved.
+ * 
+ * This source code is distributed under the GNU Lesser 
+ * General Public License, the text of which is available at:
+ *   http://www.gnu.org/copyleft/lesser.html
+ */
+
+import java.io.File;
+
+/**
+ * Utility class for manipulating file names 
+ *
+ * @author Stephen Ramsey
+ */
+public class FileUtils
+{
+    public static String getExtension(String pFileName)
+    {
+        int lastIndex = pFileName.lastIndexOf('.');
+        String retExtension = null;
+        if(-1 != lastIndex)
+        {
+            retExtension = pFileName.substring(lastIndex, pFileName.length());
+            System.out.println("found extension: " + retExtension);
+        }
+        return(retExtension);
+    }
+
+    public static String removeExtension(String pFileName)
+    {
+        int lastIndex = pFileName.lastIndexOf('.');
+        String retFileName = pFileName;
+        if(-1 != lastIndex)
+        {
+            retFileName = pFileName.substring(0, lastIndex - 1);
+        }
+        return(retFileName);
+    }
+
+    public static String createFileURL(File pFile)
+    {
+        String fileName = pFile.getAbsolutePath();
+
+        // for non-Unix operating systems, convert path separator first
+        fileName = fileName.replace(File.separatorChar, '/');
+
+        if(fileName.charAt(0) != '/')
+        {
+            fileName = "/" + fileName;
+        }
+
+        fileName = fileName.replaceAll("%", "%25");
+        fileName = fileName.replaceAll(" ", "%20");
+        fileName = fileName.replaceAll("\\:", "%3A");
+        fileName = fileName.replaceAll("#", "%23");
+        fileName = fileName.replaceAll("\\$", "%24");
+        fileName = fileName.replaceAll("&", "%26");
+        fileName = fileName.replaceAll("\\?", "%3F");
+        fileName = fileName.replaceAll("@", "%40");
+        fileName = fileName.replaceAll(";", "%3B");
+        fileName = fileName.replaceAll(",", "%2C");
+        fileName = fileName.replaceAll("\\+", "%2C");
+        fileName = fileName.replaceAll("<", "%3C");
+        fileName = fileName.replaceAll(">", "%3E");
+        fileName = fileName.replaceAll("\"", "%34");
+
+        return("file:" + fileName);
+    }
+}
