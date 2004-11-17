02ee744b94c718176b019ff1fcaebbb8a9eb3140
Stephen Ramsey <stephen.ramsey@oregonstate.edu>
2004-11-17 21:54:08 +0000

    added new sub-menus for View and Export
    git-svn-id: file:///Users/sramsey/Desktop/DizzyRepo/ISBJava_Dizzy@2819 5021c4a6-a05a-4b22-8634-7b27b77db61f

--- ISBJava_Dizzy/trunk/bolouri/java/org/systemsbiology/chem/app/MainMenu.java
+++ ISBJava_Dizzy/trunk/bolouri/java/org/systemsbiology/chem/app/MainMenu.java
@@ -33,11 +33,10 @@ public class MainMenu extends JMenuBar
     private static final String ACTION_COMMAND_HELP_BROWSER = "Browse help...";
 
     private static final String ACTION_COMMAND_TOOLS_EXPORT = "Export...";
+    private static final String ACTION_COMMAND_TOOLS_VIEW = "View...";
     private static final String ACTION_COMMAND_TOOLS_SIMULATE = "Simulate...";
     private static final String ACTION_COMMAND_TOOLS_RELOAD = "Reload model";
-    private static final String ACTION_COMMAND_TOOLS_CYTOSCAPE = "View in Cytoscape";
-    private static final String ACTION_COMMAND_TOOLS_HUMAN_READABLE = "View in human-readable format";
-
+    
     public static class Menu
     {
         private final String mName;
@@ -72,27 +71,52 @@ public class MainMenu extends JMenuBar
     public static class MenuItem
     {
         private final String mName;
+        private MenuItem []mSubMenu;
+        private ActionListener mListener;
+        
         public String toString()
         {
             return(mName);
         }
-        private static final HashMap mInstanceMap;
+        private static final HashMap sInstanceMap;
         static
         {
-            mInstanceMap = new HashMap();
+            sInstanceMap = new HashMap();
         }
         private MenuItem(String pName)
         {
-            mName = pName;
-            mInstanceMap.put(pName, this);
+            this(pName, false);
         }
-        public MenuItem get(String pName)
+        private MenuItem(String pName, boolean pHidden)
         {
-            return((MenuItem) mInstanceMap.get(pName));
+            mName = pName;
+            mSubMenu = null;
+            mListener = null;
+            if(! pHidden)
+            {
+                sInstanceMap.put(pName, this);
+            }
+        }
+        public static MenuItem get(String pName)
+        {
+            return((MenuItem) sInstanceMap.get(pName));
         }
         public String getName()
         {
             return(mName);
+        }
+        public void setSubMenu(MenuItem []pSubMenu, ActionListener pListener)
+        {
+            mSubMenu = pSubMenu;
+            mListener = pListener;
+        }
+        public MenuItem []getSubMenu()
+        {
+            return mSubMenu;
+        }
+        public ActionListener getListener()
+        {
+            return mListener;
         }
         public static final MenuItem FILE_OPEN = new MenuItem(ACTION_COMMAND_FILE_OPEN);
         public static final MenuItem FILE_SAVE_AS = new MenuItem(ACTION_COMMAND_FILE_SAVE_AS);
@@ -105,13 +129,29 @@ public class MainMenu extends JMenuBar
         public static final MenuItem TOOLS_EXPORT = new MenuItem(ACTION_COMMAND_TOOLS_EXPORT);
         public static final MenuItem TOOLS_SIMULATE = new MenuItem(ACTION_COMMAND_TOOLS_SIMULATE);
         public static final MenuItem TOOLS_RELOAD = new MenuItem(ACTION_COMMAND_TOOLS_RELOAD);
-        public static final MenuItem TOOLS_CYTOSCAPE = new MenuItem(ACTION_COMMAND_TOOLS_CYTOSCAPE);
-        public static final MenuItem TOOLS_HUMAN_READABLE = new MenuItem(ACTION_COMMAND_TOOLS_HUMAN_READABLE);
+        public static final MenuItem TOOLS_VIEW = new MenuItem(ACTION_COMMAND_TOOLS_VIEW);
         public static final MenuItem HELP_ABOUT = new MenuItem(ACTION_COMMAND_HELP_ABOUT);
         public static final MenuItem HELP_BROWSER = new MenuItem(ACTION_COMMAND_HELP_BROWSER);
     }
 
-
+    class ExportMenuListener implements ActionListener
+    {
+        public void actionPerformed(ActionEvent pEvent)
+        {
+            String actionCommand = pEvent.getActionCommand();
+            mApp.handleExport(actionCommand);
+        }
+    }
+    
+    class ViewMenuListener implements ActionListener
+    {
+        public void actionPerformed(ActionEvent pEvent)
+        {
+            String actionCommand = pEvent.getActionCommand();
+            mApp.handleView(actionCommand);
+        }        
+    }
+    
     class MenuListener implements ActionListener
     {
         public void actionPerformed(ActionEvent pEvent)
@@ -145,10 +185,6 @@ public class MainMenu extends JMenuBar
             {
                 mApp.handleHelpBrowser();
             }
-            else if(actionCommand.equals(ACTION_COMMAND_TOOLS_EXPORT))
-            {
-                mApp.handleExport();
-            }
             else if(actionCommand.equals(ACTION_COMMAND_TOOLS_SIMULATE))
             {
                 mApp.handleSimulate();
@@ -156,10 +192,6 @@ public class MainMenu extends JMenuBar
             else if(actionCommand.equals(ACTION_COMMAND_TOOLS_RELOAD))
             {
                 mApp.handleReload();
-            }
-            else if(actionCommand.equals(ACTION_COMMAND_TOOLS_CYTOSCAPE))
-            {
-                mApp.handleViewInCytoscape();
             }
             else if(actionCommand.equals(ACTION_COMMAND_EDIT_CUT))
             {
@@ -173,29 +205,52 @@ public class MainMenu extends JMenuBar
             {
                 mApp.getEditorPane().handlePaste();
             }
-            else if(actionCommand.equals(ACTION_COMMAND_TOOLS_HUMAN_READABLE))
+            else
             {
-                mApp.handleViewModelHumanReadable();
+                throw new IllegalStateException("Unknown command: " + actionCommand);
             }
         }
     }
 
-    private void createSingleMenu(Menu pMenu, MenuItem []pMenuItems, int []pShortcuts)
+
+    
+    private JMenu createSingleMenu(String pMenuName, MenuItem []pMenuItems, int []pShortcuts, ActionListener pListener)
     {
-        String menuName = pMenu.getName();
-        JMenu menu = new JMenu(menuName);
-        mMenuMap.put(menuName, menu);
+        JMenu menu = new JMenu(pMenuName);
+        mMenuMap.put(pMenuName, menu);
         for(int i = 0; i < pMenuItems.length; ++i)
         {
             MenuItem menuItem = pMenuItems[i];
+            MenuItem []subMenu = menuItem.getSubMenu();
             String menuItemName = menuItem.getName();
-            JMenuItem item = new JMenuItem(menuItemName, pShortcuts[i]);
-            item.addActionListener(new MenuListener());
-            mMenuItemMap.put(menuItemName, item);
-            menu.add(item);
+            if(null == subMenu)
+            {
+                JMenuItem item = null;
+                if(null != pShortcuts)
+                {
+                    item = new JMenuItem(menuItemName, pShortcuts[i]);
+                }
+                else
+                {
+                    item = new JMenuItem(menuItemName);
+                }
+                item.addActionListener(pListener);
+                mMenuItemMap.put(menuItemName, item);
+                menu.add(item);
+            }
+            else
+            {
+                ActionListener listener = menuItem.getListener();
+                JMenu subMenuJ = createSingleMenu(menuItemName,
+                                                  subMenu,
+                                                  null,
+                                                  listener);
+                mMenuItemMap.put(menuItemName, subMenuJ);
+                menu.add(subMenuJ);
+            }
         }
 
-        add(menu);
+        return menu;
     }
 
     synchronized void setEnabledFlag(MenuItem pMenuItem, boolean pEnabled) throws DataNotFoundException
@@ -203,7 +258,7 @@ public class MainMenu extends JMenuBar
         String menuItemName = pMenuItem.getName();
         assert (null != menuItemName) : "invalid (null) menu item name";
 
-        JMenuItem menuItem = (JMenuItem) mMenuItemMap.get(menuItemName);
+        JComponent menuItem = (JComponent) mMenuItemMap.get(menuItemName);
         if(null == menuItem)
         {
             throw new DataNotFoundException("unable to find menu item name: " + menuItemName);
@@ -224,11 +279,35 @@ public class MainMenu extends JMenuBar
         menu.setEnabled(pEnabled);
     }
 
+    private MenuItem []createRegistrySubMenu(String pName, ClassRegistry pRegistry)
+    {
+        MenuItem []retMenu = null;
+        
+        Set aliases = pRegistry.getRegistryAliasesCopy();
+        int numAliases = aliases.size();
+        if(numAliases > 0)
+        {
+            JMenu exportMenu = new JMenu(pName);
+            LinkedList aliasesList = new LinkedList(aliases);
+            Collections.sort(aliasesList);
+            String []aliasesArray = new String[numAliases];
+            retMenu = new MenuItem[numAliases];
+
+            for(int i = 0; i < numAliases; ++i)
+            {
+                String alias = (String) aliasesList.get(i);
+                retMenu[i] = new MenuItem(alias, true);
+            }
+        }
+        return retMenu;
+    }
+    
     private void createMenu() throws DataNotFoundException
     {
         mMenuMap = new HashMap();
         mMenuItemMap = new HashMap();
-
+        ActionListener listener = new MenuListener();
+        
         MenuItem []fileItems = new MenuItem[] {MenuItem.FILE_OPEN,
                                                MenuItem.FILE_SAVE,
                                                MenuItem.FILE_SAVE_AS,
@@ -236,29 +315,38 @@ public class MainMenu extends JMenuBar
                                                MenuItem.FILE_QUIT};
         int []fileShortcuts = {'O', 'S', 'A', 'C', 'Q'};
         
-        createSingleMenu(Menu.FILE, fileItems, fileShortcuts);
+        add(createSingleMenu(Menu.FILE.getName(), fileItems, fileShortcuts, listener));
 
         MenuItem []editItems = new MenuItem[] {MenuItem.EDIT_CUT,
                                                MenuItem.EDIT_COPY,
                                                MenuItem.EDIT_PASTE};
         int []editShortcuts = {'T', 'C', 'P'};
 
-        createSingleMenu(Menu.EDIT, editItems, editShortcuts);
+        add(createSingleMenu(Menu.EDIT.getName(), editItems, editShortcuts, listener));
 
-        MenuItem []toolsItems = new MenuItem[] {MenuItem.TOOLS_EXPORT,
+        MenuItem []exportSubMenu = createRegistrySubMenu("Export", mApp.getModelExporterRegistry());
+        ExportMenuListener exportListener = new ExportMenuListener();
+        MenuItem exportMenuItem = MenuItem.get(ACTION_COMMAND_TOOLS_EXPORT);
+        exportMenuItem.setSubMenu(exportSubMenu, exportListener);
+        
+        MenuItem []viewSubMenu = createRegistrySubMenu("View", mApp.getModelViewerRegistry());
+        ViewMenuListener viewListener = new ViewMenuListener();
+        MenuItem viewMenuItem = MenuItem.get(ACTION_COMMAND_TOOLS_VIEW);
+        viewMenuItem.setSubMenu(viewSubMenu, viewListener);
+        
+        MenuItem []toolsItems = new MenuItem[] {exportMenuItem,
                                                 MenuItem.TOOLS_SIMULATE,
                                                 MenuItem.TOOLS_RELOAD,
-                                                MenuItem.TOOLS_CYTOSCAPE,
-                                                MenuItem.TOOLS_HUMAN_READABLE};
-        int []toolsShortcuts = {'E', 'S', 'R', 'C', 'H'};
+                                                viewMenuItem};
+        int []toolsShortcuts = {'E', 'S', 'R', 'V'};
 
-        createSingleMenu(Menu.TOOLS, toolsItems, toolsShortcuts);
+        add(createSingleMenu(Menu.TOOLS.getName(), toolsItems, toolsShortcuts, listener));
 
         MenuItem []helpItems = new MenuItem[] {MenuItem.HELP_ABOUT,
                                                MenuItem.HELP_BROWSER};
         int []helpShortcuts = {'A', 'B'};
 
-        createSingleMenu(Menu.HELP, helpItems, helpShortcuts);
+        add(createSingleMenu(Menu.HELP.getName(), helpItems, helpShortcuts, listener));
         
 
     }
