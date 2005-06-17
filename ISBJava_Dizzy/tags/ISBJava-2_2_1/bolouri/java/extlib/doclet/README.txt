205e208cb92196b5aab32cf64b9b616da49144f6
No Author <nobody@nowhere.com>
2005-06-17 21:26:40 +0000

    This commit was manufactured by cvs2svn to create tag 'ISBJava-2_2_1'.
    git-svn-id: file:///Users/sramsey/Desktop/DizzyRepo/ISBJava_Dizzy@3260 5021c4a6-a05a-4b22-8634-7b27b77db61f

---
+++ ISBJava_Dizzy/tags/ISBJava-2_2_1/bolouri/java/extlib/doclet/README.txt
@@ -0,0 +1,3 @@
+This directory contains JAR files thare are
+needed in order to generate the Javadoc
+documentation for the ISBJava library.
---
+++ ISBJava_Dizzy/tags/ISBJava-2_2_1/bolouri/java/org/systemsbiology/inference/EvidenceWeightedInfererParams.java
@@ -0,0 +1,118 @@
+/*
+ * Copyright (C) 2005 by Institute for Systems Biology,
+ * Seattle, Washington, USA.  All rights reserved.
+ * 
+ * This source code is distributed under the GNU Lesser
+ * General Public License, the text of which should have
+ * been distributed with this source code in the file 
+ * License.html.  The license can also be obtained at:
+ *   http://www.gnu.org/copyleft/lesser.html
+ */
+package org.systemsbiology.inference;
+
+/**
+ * Data structure that holds the parameters that are
+ * passed to the {@link EvidenceWeightedInferer}, to describe
+ * how the Pointillist algorithm should run.  The parameters 
+ * include the number of bins used to calculate the nonparametric
+ * distribution, the initial cutoff for the evidence-specific
+ * significances, the final cutoff for the combined significances,
+ * the smoothing length for the log-ignificance distribution,
+ * and the type of formula to be used to compute the evidence-specific
+ * weights.
+ * 
+ * @author sramsey
+ *
+ */
+public class EvidenceWeightedInfererParams
+{
+    private Integer mNumBins;
+    private Double mInitialSignificanceCutoff;
+    private Double mCombinedSignificanceQuantileCutoff;
+    private Double mFractionToRemove;
+    private Double mMinFractionalCostChange;
+    private Double mSmoothingLength;
+    private Integer mMaxIterations;
+    private EvidenceWeightType mEvidenceWeightType;
+    
+    public void setMaxIterations(Integer pMaxIterations)
+    {
+        mMaxIterations = pMaxIterations;
+    }
+    
+    public Integer getMaxIterations()
+    {
+        return mMaxIterations;
+    }
+    
+    public void setNumBins(Integer pNumBins)
+    {
+        mNumBins = pNumBins;
+    }
+    
+    public Integer getNumBins()
+    {
+        return mNumBins;
+    }
+    
+    public void setInitialSignificanceCutoff(Double pInitialSignificanceCutoff)
+    {
+        mInitialSignificanceCutoff = pInitialSignificanceCutoff;
+    }
+    
+    public Double getInitialSignificanceCutoff()
+    {
+        return mInitialSignificanceCutoff;
+    }
+    
+    public void setCombinedSignificanceCutoff(Double pCombinedSignificanceQuantileCutoff)
+    {
+        mCombinedSignificanceQuantileCutoff = pCombinedSignificanceQuantileCutoff;
+    }
+    
+    public Double getCombinedSignificanceCutoff()
+    {
+        return mCombinedSignificanceQuantileCutoff;
+    }
+    
+    public void setFractionToRemove(Double pFractionToRemove)
+    {
+        mFractionToRemove = pFractionToRemove;
+    }
+    
+    public Double getFractionToRemove()
+    {
+        return mFractionToRemove;
+    }
+    
+    public void setMinFractionalCostChange(Double pMinFractionalCostChange)
+    {
+        mMinFractionalCostChange = pMinFractionalCostChange;
+    }
+
+    public Double getMinFractionalCostChange()
+    {
+        return mMinFractionalCostChange;
+    }
+    
+    public void setSmoothingLength(Double pSmoothingLength)
+    {
+        mSmoothingLength = pSmoothingLength;
+    }
+    
+    public Double getSmoothingLength()
+    {
+        return mSmoothingLength;
+    }
+    
+    public void setEvidenceWeightType(EvidenceWeightType pEvidenceWeightType)
+    {
+        mEvidenceWeightType = pEvidenceWeightType;
+    }
+    
+    public EvidenceWeightType getEvidenceWeightType()
+    {
+        return mEvidenceWeightType;
+    }
+    
+}
