4a90639ff83fc588b0e9e7ae9d2e73fdbb61dea8
No Author <nobody@nowhere.com>
2006-03-13 18:41:07 +0000

    This commit was manufactured by cvs2svn to create tag 'Pointillist-1_0_3'.
    git-svn-id: file:///Users/sramsey/Desktop/DizzyRepo/ISBJava_Dizzy@3399 5021c4a6-a05a-4b22-8634-7b27b77db61f

---
+++ ISBJava_Dizzy/tags/Pointillist-1_0_3/bolouri/apps/Pointillist/config/AppHelp.hs
@@ -0,0 +1,31 @@
+<?xml version="1.0" encoding="ISO-8859-1"?>
+<!DOCTYPE helpset
+  PUBLIC "-//Sun Microsystems Inc.//DTD JavaHelp HelpSet Version 1.0//EN"
+  "http://java.sun.com/products/javahelp/helpset_1_0.dtd" 
+  [
+  <!ENTITY % AppConfig SYSTEM "../config/AppConfig.dtd">
+  %AppConfig;
+  ]>
+
+<helpset version="1.0">
+  <title>&appName; Help</title>
+
+  <maps>
+    <homeID>top</homeID>
+    <mapref location="AppHelp.jhm" />
+  </maps>
+
+  <view>
+    <name>TOC</name>
+    <label>Table of Contents</label>
+    <type>javax.help.TOCView</type>
+    <data>AppHelpTOC.xml</data>
+  </view>
+
+  <view>
+    <name>Search</name>
+    <label>Search</label>
+    <type>org.systemsbiology.gui.ExHelpSearchNavigator</type> 
+    <data engine="com.sun.java.help.search.DefaultSearchEngine">JavaHelpSearch</data>
+  </view>
+</helpset>
